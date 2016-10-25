403a458d1fa67159aae706d0c15177886ed5a4d3
Chin Godawita <chin@sitback.com.au>
2016-10-25 17:08:37 +1100

    Fixed issue with accessing htmlSafe.

--- addon/components/background-image.js
+++ addon/components/background-image.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import layout from '../templates/components/background-image';
 import ImageLoaderMixin from '../mixins/image-loader-mixin';
 
+const { Component, String: EmberString, on } = Ember;
+
 /**
   Loads a stateful image for its css background-image.
   Class names are updated according to the image state.
@@ -11,15 +13,15 @@ import ImageLoaderMixin from '../mixins/image-loader-mixin';
   @uses ImageLoaderMixin
   @public
 **/
-export default Ember.Component.extend(ImageLoaderMixin, {
+export default Component.extend(ImageLoaderMixin, {
   layout,
   attributeBindings: ['style'],
   classNames: ['background-image'],
 
-  applyStyle: Ember.on('willLoad', function(url) {
+  applyStyle: on('willLoad', function(url) {
     if (url) {
       let backgroundImageStyle = `background-image:url("${url}")`;
-      this.set('style', backgroundImageStyle.htmlSafe());
+      this.set('style', EmberString.htmlSafe(backgroundImageStyle));
     }
   })
 });
